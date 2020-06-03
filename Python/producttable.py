57c4a1f1a0642a5a3d8df1b8531c42c99aad7b79
CaptainLukas <Lukas.Forsthuber@gmx.at>
2020-06-03 20:17:48 +0200

    Produkttable init

---
+++ Python/producttable.py
@@ -0,0 +1,8 @@
+class Producttable:
+    __tablename__='producttable'
+    id = Column(Integer, primary_key=True)
+    on_id = Column('on_id', String(32))
+    orderdate=Column('orderdate',datetime)
+    qtr_id=Column('qtr_id',int)
+    month_id=Column('month_id',int)
+    year_id=Column('year_id',int)
